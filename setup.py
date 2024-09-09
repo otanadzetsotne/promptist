d5b9a2deb6b8bd18e214ee0045974eeca32ce49e
otanadzetsotne <otanadzetsotne@yahoo.com>
2024-09-09 14:30:18 +0400

    author email

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='promptist',
-    version='0.1.1',
-    packages=find_packages(),        # Автоматически найдет все пакеты и модули
+    version='0.1.2',
+    packages=find_packages(),
     install_requires=[
         'annotated-types==0.7.0',
         'pydantic==2.9.0',
@@ -12,13 +12,13 @@ setup(
         'typing_extensions==4.12.2',
         'tzdata==2024.1',
     ],
-    author='Tsotne Otanadze',              # Автор
-    author_email='your_email@example.com',  # Email автора
+    author='Tsotne Otanadze',
+    author_email='otanadzetsotne@yahoo.com',
     description='Short description of your package',
     long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',  # Тип содержания README (Markdown)
-    url='https://github.com/otanadzetsotne/promptist',  # URL проекта, если есть
-    classifiers=[                   # Классификаторы (метаданные)
+    long_description_content_type='text/markdown',
+    url='https://github.com/otanadzetsotne/promptist',
+    classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
